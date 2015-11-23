5bd3b06bdf2933531197eae8c62f27f59582bc37
Sascha Jovanoski <info@sajo-media.de>
2015-11-23 18:12:02 +0100

    add test coreAdmin

--- bin/install-solr.sh
+++ bin/install-solr.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
-wget http://artfiles.org/apache.org/lucene/solr/5.3.1/solr-5.3.1.tgz
-tar xfz solr-5.3.1.tgz
+
+if [[ $# -eq 0 ]] ; then
+    echo 'solr version as argument 5.3.1'
+    exit 1
+fi
+
+FILE="solr-$1.tgz"
+
+if [ -f $FILE ];
+then
+  echo "File $FILE exists."
+else
+  URL="http://artfiles.org/apache.org/lucene/solr/$1/$FILE"
+  echo "File $FILE does not exist. download $URL"
+  wget $URL
+  tar xfz $FILE
+  rm $FILE
+fi
+
 cd solr-5.3.1
+echo "Start Solr"
 bin/solr start
+
