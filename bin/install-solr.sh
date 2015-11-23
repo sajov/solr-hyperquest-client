fbb19b65674e4b78d5982a4d94a19e5485723715
Sascha Jovanoski <info@sajo-media.de>
2015-11-23 18:50:22 +0100

    fix solr installation

--- bin/install-solr.sh
+++ bin/install-solr.sh
@@ -1,24 +1,9 @@
 #!/bin/sh
 
-if [[ $# -eq 0 ]] ; then
-    echo 'solr version as argument 5.3.1'
-    exit 1
-fi
+FILE="solr-5.3.1.tgz"
+URL="http://artfiles.org/apache.org/lucene/solr/5.3.1/"
 
-FILE="solr-$1.tgz"
-
-if [ -f $FILE ];
-then
-  echo "File $FILE exists."
-else
-  URL="http://artfiles.org/apache.org/lucene/solr/$1/$FILE"
-  echo "File $FILE does not exist. download $URL"
-  wget $URL
-  tar xfz $FILE
-  rm $FILE
-fi
-
+wget $URL$FILE
+tar xfz solr-5.3.1.tgz
 cd solr-5.3.1
-echo "Start Solr"
 bin/solr start
-
